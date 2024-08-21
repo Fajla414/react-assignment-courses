7ddc66024c5309bc76f489be09243710e9d0daa7
Fajla Rabby <blfajlarabby414@gmail.com>
2024-08-21 08:30:00 +0600

    first commit

---
+++ src/component/data/data.js
@@ -0,0 +1,100 @@
+const DATA =[
+    {
+        id: 1,
+        image: 'assets/images/arqitecture.png',
+        price: '1500',
+        classDuration: '01:20m',
+        courseName: 'Arqitecture',
+        courseDuration: "6-months"
+    },
+    {
+        id: 2,
+        image: 'assets/images/cooking.png',
+        price: '500',
+        classDuration: '30m',
+        courseName: 'Cooking',
+        courseDuration: "1-months"
+    },
+    {
+        id: 3,
+        image: 'assets/images/graphicDesign.png',
+        price: '1500',
+        classDuration: '01:00m',
+        courseName: 'Graphics Design',
+        courseDuration: "6-months"
+    },
+    {
+        id: 4,
+        image: 'assets/images/Illustrator.png',
+        price: '1500',
+        classDuration: '01:00m',
+        courseName: 'Adobe Illustrato',
+        courseDuration: "6-months"
+    },
+    {
+        id: 5,
+        image: 'assets/images/javaScript.png',
+        price: '2000',
+        classDuration: '50m',
+        courseName: 'JavaScript',
+        courseDuration: "6-months"
+    },
+    {
+        id: 6,
+        image: 'assets/images/modernPsychology.png',
+        price: '1000',
+        classDuration: '30m',
+        courseName: 'Modern Psychology',
+        courseDuration: "3-months"
+    },
+    {
+        id: 7,
+        image: 'assets/images/photoshop.png',
+        price: '1500',
+        classDuration: '01:00m',
+        courseName: 'Adobe Photoshop',
+        courseDuration: "6-months"
+    },
+    {
+        id: 1,
+        image: 'assets/images/python.png',
+        price: '2500',
+        classDuration: '01:00m',
+        courseName: 'Python',
+        courseDuration: "6-months"
+    },
+    {
+        id: 9,
+        image: 'assets/images/react.png',
+        price: '2000',
+        classDuration: '01:00m',
+        courseName: 'React',
+        courseDuration: "6-months"
+    },
+    {
+        id: 10,
+        image: 'assets/images/sql.png',
+        price: '2500',
+        classDuration: '01:00m',
+        courseName: 'SQL',
+        courseDuration: "6-months"
+    },
+    {
+        id: 11,
+        image: 'assets/images/webDevelopment.png',
+        price: '3000',
+        classDuration: '01:20m',
+        courseName: 'Web Development',
+        courseDuration: "6-months"
+    },
+    {
+        id: 12,
+        image: 'assets/images/webDesign.png',
+        price: '1200',
+        classDuration: '50m',
+        courseName: 'Web Design',
+        courseDuration: "3-months"
+    },
+]
+
+export default DATA;
